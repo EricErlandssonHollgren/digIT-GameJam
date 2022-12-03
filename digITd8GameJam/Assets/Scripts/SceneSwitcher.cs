3a2592f62aefde8c4e816dbb60664cb71d4bff95
EricErlandssonHollgren <eric.e.hollgren@gmail.com>
2022-12-03 18:40:48 +0100

    yes

--- digITd8GameJam/Assets/Scripts/SceneSwitcher.cs
+++ digITd8GameJam/Assets/Scripts/SceneSwitcher.cs
@@ -9,15 +9,6 @@ public class SceneSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            Debug.Log(SceneManager.GetActiveScene().name);
-            if (SceneManager.GetActiveScene().buildIndex == 0)
-            {
-                SceneManager.LoadScene(1);
-            } else {
-                SceneManager.LoadScene(0);
-            }
-        }
+        
     }
 }
