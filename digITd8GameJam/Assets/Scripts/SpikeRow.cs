797c506cc64e352a73c5579b9a5b9ea64af0b02b
LilleLink <simon_0_0_@live.se>
2022-12-04 13:29:21 +0100

    Prefab spikes

---
+++ digITd8GameJam/Assets/Scripts/SpikeRow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeRow : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject target = collision.gameObject;
+        target.GetComponent<PlayerController>().removeHP(100000);
+    }
+}
